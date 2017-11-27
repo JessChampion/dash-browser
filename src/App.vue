347e0d6447bee271b16c09eb62264cbaed166905
Jess Champion <jess@silverstripe.com>
2017-11-27 17:14:03 +1300

    Base setup

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="app">
+    <Block v-bind:data="rootBlock"/>
+  </div>
+</template>
+
+<script>
+  import Block from './components/Block';
+
+  export default {
+    name: 'app',
+    components: {
+      Block,
+    },
+    data() {
+      return {
+        rootBlock: {
+          id: 1,
+          name: 'Block 1',
+          vertical: true,
+          color: null,
+          children: [
+            {
+              id: 2,
+              name: 'Block 1',
+              vertical: true,
+              color: 'blue',
+              children: null
+            }, {
+              id: 3,
+              name: 'Block 1',
+              vertical: true,
+              color: null,
+              children: [{
+                id: 4,
+                name: 'Block 4',
+                vertical: true,
+                color: 'blue',
+                children: null
+              }, {
+                id: 5,
+                name: 'Block 5',
+                vertical: true,
+                color: 'pink',
+                children: null
+              }]
+            }
+          ]
+        }
+      };
+    }
+  };
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
---
+++ src/components/Block.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="hello">
+    {{name}}
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Block',
+    props: ['data'],
+    data() {
+      //is leaf node
+      if (this.data.children && this.data.children.length > 0) {
+        return {
+          isLeaf: false,
+          children: this.data.children,
+          vertical: true
+        };
+      }
+      return {
+        isLeaf: false,
+        color: 'blue',
+        vertical: true,
+      };
+    },
+  };
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1, h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
+</style>
